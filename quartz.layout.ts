471a37e61063f86e78aa4ae0ae8573dccdf354e4
Silver_Joker <modsifisticated@gmail.com>
2026-03-29 19:41:54 +1100

    Quartz sync: Mar 29, 2026, 7:41 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,12 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs({
+  spacerSymbol: "❯", // symbol between crumbs
+  rootName: "Home", // name of first/root element
+  resolveFrontmatterTitle: true, // whether to resolve folder names through frontmatter titles
+  showCurrentPage: true, // whether to display the current page in the breadcrumbs
+}), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
